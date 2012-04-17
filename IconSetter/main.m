b72126def18a204162be2e02c901b48f4a8e8835
Tobias Haag <tha@cs.stir.ac.uk>
2012-04-17 12:09:32 +0200

    command line tool to set icons

--- IconSetter/main.m
+++ IconSetter/main.m
@@ -6,65 +6,186 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-static NSManagedObjectModel *managedObjectModel()
-{
-    static NSManagedObjectModel *model = nil;
-    if (model != nil) {
-        return model;
-    }
+
+
+@interface IconManager : NSObject
+
+- (NSImage*) getBadgeForState: (NSString*) state AndIconPath: (NSString*) path;
+- (NSImage*) getIconWithBadge: (NSImage*)icon AndState: (NSString*)state AndIconPath:(NSString*) path;
+- (NSImage*) getCurrentIcon: (NSString*) file;
+
+- (BOOL) setIconForFile: (NSString*) file AndIconPath: (NSString*) path AndState: (NSString*) state;
+
+- (BOOL) replaceIconOfFile: (NSString*) file AndImageFile: (NSString*) imageFile;
+@end
+
+@implementation IconManager{
     
-    NSString *path = [[[NSProcessInfo processInfo] arguments] objectAtIndex:0];
-    path = [path stringByDeletingPathExtension];
-    NSURL *modelURL = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"momd"]];
-    model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    
-    return model;
 }
 
-static NSManagedObjectContext *managedObjectContext()
-{
-    static NSManagedObjectContext *context = nil;
-    if (context != nil) {
-        return context;
+- (BOOL) replaceIconOfFile:(NSString *)file AndImageFile:(NSString *)imageFile{
+    
+    if([[NSFileManager defaultManager] fileExistsAtPath:imageFile]){
+        NSImage *newIcon = [[NSImage new] initWithContentsOfFile:imageFile];
+        
+        [[NSWorkspace sharedWorkspace] setIcon:newIcon forFile:file options:0];
+         return YES;
     }
-
-    @autoreleasepool {
-        context = [[NSManagedObjectContext alloc] init];
-        
-        NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:managedObjectModel()];
-        [context setPersistentStoreCoordinator:coordinator];
-        
-        NSString *STORE_TYPE = NSSQLiteStoreType;
-        
-        NSString *path = [[[NSProcessInfo processInfo] arguments] objectAtIndex:0];
-        path = [path stringByDeletingPathExtension];
-        NSURL *url = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"sqlite"]];
-        
-        NSError *error;
-        NSPersistentStore *newStore = [coordinator addPersistentStoreWithType:STORE_TYPE configuration:nil URL:url options:nil error:&error];
-        
-        if (newStore == nil) {
-            NSLog(@"Store Configuration Failure %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
-        }
-    }
-    return context;
+         
+         return NO;
+    
+    
 }
+
+- (BOOL) setIconForFile:(NSString *)file AndIconPath:(NSString *)path AndState:(NSString *)state{
+    
+    NSImage* oldIcon = [self getCurrentIcon:file];
+    
+    NSImage *finalIcon = [self getIconWithBadge: oldIcon AndState: state
+                                  AndIconPath:path];
+    
+    
+    if(finalIcon == nil) return NO;
+    
+    [[NSWorkspace sharedWorkspace] setIcon:finalIcon forFile:file options:0];
+    
+    return YES;
+}
+
+- (NSImage*) getCurrentIcon:(NSString *)file{
+    
+    [[NSWorkspace sharedWorkspace] setIcon:nil forFile:file options:0];
+    NSImage *iconImg = [[NSWorkspace sharedWorkspace] iconForFile:file];
+    
+    [iconImg setScalesWhenResized:YES];
+    
+    NSImageRep *biggestRep = [[iconImg representations] objectAtIndex:0];
+    
+    
+    [iconImg setSize:NSMakeSize(biggestRep.size.width,biggestRep.size.height)];
+    
+    return iconImg;
+}
+
+- (NSImage*) getBadgeForState: (NSString*) state AndIconPath: (NSString*) path{
+    NSString *badge;
+    
+    if([@"notSynced" isEqualToString:state]){
+        badge = [path stringByAppendingString:@"/notSynced.png"];
+    }else if([@"synced" isEqualToString:state]){
+        badge = [path stringByAppendingString:@"/synced.png"];
+    }else if([@"syncing" isEqualToString:state]){
+        badge = [path stringByAppendingString:@"/syncing.png"];
+    }
+    
+    NSImage *badgeImg = [[NSImage new] initWithContentsOfFile:badge];
+    
+    if(badge == nil){ return nil;}
+    
+    return badgeImg;
+    
+    
+    return nil;
+    
+}
+
+
+- (NSImage*) getIconWithBadge: (NSImage*)icon AndState: (NSString*)state AndIconPath:(NSString*) path
+{
+    
+    NSImage *badge = [self getBadgeForState:state AndIconPath:path];
+    
+    if(badge == nil) return nil;
+    
+    NSSize newBadgeSize = NSMakeSize(icon.size.width/2, icon.size.height/2);
+    
+    [badge setSize:newBadgeSize];
+    
+    
+    [icon lockFocus];
+    [badge drawInRect:NSMakeRect(icon.size.width - badge.size.width, 0, badge.size.width, badge.size.height) 
+             fromRect:NSZeroRect 
+            operation:NSCompositeSourceOver
+             fraction:1.0];
+    [icon unlockFocus];
+    
+    
+    
+    return icon;
+}
+
+@end
+
 
 int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        // Create the managed object context
-        NSManagedObjectContext *context = managedObjectContext();
+    NSAutoreleasePool *pool;
+    pool = [[NSAutoreleasePool alloc] init];
+    
+    NSString *file;
+    NSString *iconPath;
+    
+        if(argc == 4){
         
-        // Custom code here...
-        // Save the managed object context
-        NSError *error = nil;
-        if (![context save:&error]) {
-            NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
+        
+            file = [NSString stringWithUTF8String:argv[1]];
+            iconPath = [NSString stringWithUTF8String:argv[2]];
+            NSString *state = [NSString stringWithUTF8String:argv[3]];
+            
+        //NSLog(@"filename: %@ icon: %@",file,icon);
+        
+        
+            if([[IconManager new] setIconForFile:file AndIconPath:iconPath AndState:state]){
+                return 0;
+            }else{
+                printf("Icon could not be set\n");
+                exit(3);
+            
+            }
+            
+        }else if(argc == 3){
+            
+            if(strcmp(argv[1], "-r") == 0){
+                
+                file = [NSString stringWithUTF8String:argv[2]];
+                
+                if([[NSFileManager defaultManager]fileExistsAtPath:file]){
+                    [[NSWorkspace sharedWorkspace] setIcon:nil forFile:file options:0];
+                    return 0;
+                }else{
+                    printf("File not found: %s\n",argv[2]);
+                    exit(4);
+                }
+
+            }else{
+                file = [NSString stringWithUTF8String:argv[1]];
+                iconPath = [NSString stringWithUTF8String:argv[2]];
+                
+                if([[NSFileManager defaultManager] fileExistsAtPath:file] && [[NSFileManager defaultManager] fileExistsAtPath:iconPath]){
+                    
+                    if([[IconManager new] replaceIconOfFile:file AndImageFile:iconPath]){
+                        return 0;
+                    }else{
+                        printf("Could not set new icon\n");
+                        exit(3);
+                    }
+                }else{
+                    printf("File not found!\n");
+                    exit(4);
+                }
+                
+                
+            }
+        }else{
+            printf("Usage: IconSetter < < -r > <pathToFile> > | < <pathToFile> <pathToImage> > | \n\t< <pathToFile> <pathToIcons> <state> > \n");
             exit(1);
         }
-    }
-    return 0;
+    
+        
+        
+    [pool drain];
+        return 0;
+
 }
+
 
